07ae7de9459b96043d615fc392e4127ee6b4f683
berkovics <berkovicsgellert@ktch.hu>
2024-02-04 16:48:20 +0100

    16:48

--- src/app/vizsgalat/vizsgalat.component.ts
+++ src/app/vizsgalat/vizsgalat.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-//declare function greet(): void
-
 @Component({
   selector: 'app-vizsgalat',
   templateUrl: './vizsgalat.component.html',
@@ -27,64 +25,62 @@ export class VizsgalatComponent {
   redRange:any = 0
   blueRange:any = 0
 
-  constructor() {
-    //greet()
-  }
-
-  startRed() {
-    if (0<=this.r && this.r<20000) {
-      if (0<=this.r && this.r<8000) {
-        this.r++
-        this.redValue2 = "Kicsi hallom"
-      }
-      if (4000<=this.r && this.r<15000) {
-        this.r++
-        this.redValue2 = "Közepes hallom"
-      }
-      if (7000<=this.r && this.r<20000) {
-        this.r++
-        this.redValue2 = "Nagyon hallom"
-      }
-      this.r++
-      this.redRange = this.r
-      this.redValue = this.r
-    }
-  }
-
-  startBlue() {
-    if (0<=this.b && this.b<20000) {
-      if (0<=this.b && this.b<4000) {
-        this.b++
-        this.blueValue2 = "Kicsi hallom"
-      }
-      if (4000<=this.b && this.b<7000) {
-        this.b++
-        this.blueValue2 = "Közepes hallom"
-      }
-      if (7000<=this.b && this.b<20000) {
-        this.b++
-        this.blueValue2 = "Nagyon hallom"
-      }
-      this.b++
-      this.blueRange = this.b
-      this.blueValue = this.b
-    }
-  }
+  constructor() {}
 
   nohear() {
-    if (this.red == true) {
+    if (this.red = true) {
       this.r = 0
-      this.r2.setInterval(this.startRed, 1)
-    } else if (this.blue == true) {
+      this.r2 = setInterval(
+        () => {
+          if (0<=this.r && this.r<20000) {
+            if (0<=this.r && this.r<8000) {
+              this.r++
+              this.redValue2 = "Kicsi hallom"
+            }
+            if (4000<=this.r && this.r<15000) {
+              this.r++
+              this.redValue2 = "Közepes hallom"
+            }
+            if (7000<=this.r && this.r<20000) {
+              this.r++
+              this.redValue2 = "Nagyon hallom"
+            }
+            this.r++
+            this.redRange = this.r
+            this.redValue = this.r
+          }
+        }, 1
+      )
+    } else if (this.blue = true) {
       this.b = 0
-      this.b2.setInterval(this.startBlue, 1)
+      this.b2 = setInterval(
+        () => {
+          if (0<=this.b && this.b<20000) {
+            if (0<=this.b && this.b<4000) {
+              this.b++
+              this.blueValue2 = "Kicsi hallom"
+            }
+            if (4000<=this.b && this.b<7000) {
+              this.b++
+              this.blueValue2 = "Közepes hallom"
+            }
+            if (7000<=this.b && this.b<20000) {
+              this.b++
+              this.blueValue2 = "Nagyon hallom"
+            }
+            this.b++
+            this.blueRange = this.b
+            this.blueValue = this.b
+          }
+        }, 1
+      )
     }
   }
 
   hear() {
-    if (this.red == true) {
+    if (this.red = true) {
       clearInterval(this.r2)
-    } else if (this.blue == true) {
+    } else if (this.blue = true) {
       clearInterval(this.b2)
     }
   }
@@ -102,12 +98,16 @@ export class VizsgalatComponent {
   
     clearInterval(this.r2)
     clearInterval(this.b2)
+
     this.red = false
     this.blue = false
+
     this.redRange = 0
     this.redValue = 0
+
     this.blueRange = 0
     this.blueValue = 0
+
     this.redValue2 = ""
     this.blueValue2 = ""
   
