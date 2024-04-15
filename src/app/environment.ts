df7a67d3fa71569961707b0fc9720fe3f24c21a1
berkovics <berkovicsgellert@ktch.hu>
2024-04-15 14:04:12 +0200

    javitas

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -10,3 +10,14 @@ export const Environment = {
     measurementId: "G-0SXYPT1EZC"
   }
 }
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDMOl4dpdjy3AO67nK_tKiX2ET3IXPWJkM",
+  authDomain: "deaf-fdfba.firebaseapp.com",
+  databaseURL: "https://deaf-fdfba-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "deaf-fdfba",
+  storageBucket: "deaf-fdfba.appspot.com",
+  messagingSenderId: "108027907815",
+  appId: "1:108027907815:web:d5b64c958780686cee9c42",
+  measurementId: "G-0SXYPT1EZC"
+};
